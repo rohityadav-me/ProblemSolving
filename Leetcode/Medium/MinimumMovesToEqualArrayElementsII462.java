c0ea4f3fa807c8085208683f9dfb748ef0a4a5ac
Rohit Yadav <rohit.yadav102@gmail.com>
2022-06-30 14:44:27 +0530

    Leetcode 462. Minimum Moves to Equal Array Elements II solved

---
+++ Leetcode/Medium/MinimumMovesToEqualArrayElementsII462.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length-1;
+        int count = 0;
+        while(start<end){
+            count += nums[end--]-nums[start++];
+        }
+        return count;
+    }
+}
